5096f877455debdc122d119e30d95564dd79d42d
Matheus Orlandi de Castro <mo.castro@hotmail.com>
2015-12-07 22:02:45 -0200

    - banner - tela sobre

--- application/views/site/sobre.php
+++ application/views/site/sobre.php
@@ -3,7 +3,19 @@
      
 <div class="jumbotron" style="color: #fff">
     <center><h1><?php echo sprintf(('Sistema de Visualização da Análise de Água Subterrânea'), '<span class="zf-green">', '</span>') ?></h1>
-        <p><?php echo sprintf(('Sistema desenvolvido em parceria com o Departamento de Engenharia Ambiental e alunos do curso de Sistemas de Informação da Universidade Federal de Santa Maria/FW. <br> Seu objetivo é realizar uma visualização dinâmica dos dados coletados da análise de agua da região da Bacia do Rio da Várzea.')) ?></p></center>
+        <p><?php echo sprintf(('Sistema desenvolvido em parceria com o Departamento de Engenharia Ambiental e alunos do curso de Sistemas de Informação da Universidade Federal de Santa Maria/FW. <br> Seu objetivo é realizar uma visualização dinâmica dos dados coletados da análise de agua da região da Bacia do Rio da Várzea.')) ?></p>
+    </center>
+    <br>
+    <ul style="font-size: 15px">
+        Orientado pelo professor Cristiano Bertolini - cbertolini@gmail.com
+        <br>Alunos:
+        <li> Denison Molina
+        <li> Juliano Conceição
+        <li> Lazaro Martins
+        <li> Leonardo Linke
+        <li> Matheus O. de Castro
+    </ul>
+    
 
 </div>
 </body>
